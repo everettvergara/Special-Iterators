56d457d721260e2f84679ea6dea78190976839ef
Everett Gaius Vergara <evergara@shinra.com.ph>
2022-07-16 21:40:27 +0800

    fixed misc bugs and simplify

--- include/ndeep_loop.hpp
+++ include/ndeep_loop.hpp
@@ -11,7 +11,7 @@ namespace g80 {
 
         std::vector<const std::vector<T> *> data_;
         std::vector<size_t> ix_;
-        size_t d_;
+        size_t d_{0};
     
     public:
 
@@ -21,38 +21,39 @@ namespace g80 {
             ix_.emplace_back(0);
         }
 
-        inline auto ix() const -> const std::vector<const size_t> & {return ix_;}
-
-    public:
-
-        ndeep_loop() : f_(init) {}
+        inline auto ix() const -> const std::vector<size_t> & {return ix_;}
 
     private:
 
         std::function<auto () -> bool> f_;
 
-        auto invalid() -> bool;
+        auto invalid() -> bool {return false;}
+
+        auto next() -> bool {
+            // std::cout << "here1\n";
+            if (ix_[d_] != data_[d_]->size()) d_ = data_.size() - 1;
+            // std::cout << "here2\n";
+            while (++ix_[d_] == data_[d_]->size())
+                if (ix_[d_] = 0; --d_ >= data_.size()) return false;
+            // std::cout << "here3\n";
+            return true;
+        }
 
         auto init() -> bool {
-            auto set_invalid = [&]() -> bool {f_ = invalid(); return false;}
+            auto set_invalid = [&]() -> bool {f_ = std::bind(&ndeep_loop::invalid, this); return false;};
             if (data_.size() == 0) return set_invalid();
             for (auto &s : data_) if (s->size() == 0) return set_invalid();
-            f_ = next();
+            f_ = std::bind(&ndeep_loop::next, this);
             return true;
         } 
 
-        auto next() -> bool {
-            if (ix_[d_] != data_[d_]->size()) d_ = data_.size() - 1;
-            while (++ix_[d_] == data_[d_]->size())
-                if (ix_[d_] = 0; --d_ >= data_.size()) return false;
-            return true;
-        }
+    public:
+
+        auto iterate() -> bool {return f_();}
 
     public:
 
-        auto iterate() -> bool {
-            return f_();
-        }
+        ndeep_loop() : f_(std::bind(&ndeep_loop::init, this)) {}
 
     }; 
 
